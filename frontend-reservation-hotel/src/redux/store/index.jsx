c9154e1e84bddc9376715665404efbe49784d0d3
Julio Wong <julioo.wong@gmail.com>
2022-06-08 21:02:10 -0500

    commit

--- frontend-reservation-hotel/src/redux/store/index.jsx
+++ frontend-reservation-hotel/src/redux/store/index.jsx
@@ -1,18 +1,20 @@
 import { createStore, applyMiddleware } from "redux";
-
+import thunk from "redux-thunk" 
+import reducers from "../reducers"
 import {persistStore, persistReducer} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import thunk from "redux-thunk" 
-import reducers from "../reducers"
-
 const persistConfig = {
-    key: "root",
-    storage
+    key: 'authType',
+    storage: storage,
+    whitelist: ['guest']
 };
 
-const store = createStore(reducers, applyMiddleware(thunk),
+const pReducer = persistReducer(persistConfig, reducers);
+const store = createStore(pReducer, applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+);
+
+const persistor = persistStore(store);
 
 export default store;
